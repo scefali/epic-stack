b7297b9d084d3a168ad7c3024906786b44bfe5f5
Stephen Cefali <scefali@sentry.io>
2023-05-16 12:40:33 -0700

    try stuff

--- app/routes/_auth+/login.tsx
+++ app/routes/_auth+/login.tsx
@@ -44,7 +44,14 @@ export default function LoginPage() {
 		<div className="flex min-h-full flex-col justify-center pb-32 pt-20">
 			<div className="mx-auto w-full max-w-md">
 				<div className="flex flex-col gap-3 text-center">
-					<h1 className="text-h1">Welcome back!</h1>
+					<h1
+						className="text-h1"
+						onClick={() => {
+							throw new Error('lolol')
+						}}
+					>
+						Welcome back!
+					</h1>
 					<p className="text-body-md text-night-200">
 						Please enter your details.
 					</p>
